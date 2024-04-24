ad8a77d9d545f785883d6ae78b859666de49eeda
Darshan Dhande <dad86@duke.edu>
2024-04-24 18:31:01 -0400

    changed to unix time in seconds

--- app/api/fixture/route.ts
+++ app/api/fixture/route.ts
@@ -9,11 +9,18 @@ const PRIVATE_KEY = config.PRIVATE_KEY;
 const API_KEY = config.API_KEY;
 const URL = `https://cricket.sportmonks.com/api/v2.0/fixtures?filter[league_id]=1&filter[season_id]=1484&filter[status]=NS&fields[fixtures]=id,localteam_id,visitorteam_id,starting_at&api_token=${API_KEY}`;
 
-interface NextFixture {
+interface getNextFixture {
     id: number;
     localteam_id: number;
     visitorteam_id: number;
     starting_at: string;
+}
+
+interface NextFixture {
+    id: number;
+    localteam_id: number;
+    visitorteam_id: number;
+    starting_at: number;
 }
 
 async function fetchNextFixtureData() {
@@ -31,7 +38,14 @@ async function fetchNextFixtureData() {
         }
 
         const data = (await response.json()) as any;
-        const firstFixture: NextFixture = data.data[0];
+        const getFirstFixture: getNextFixture = data.data[0];
+
+        const firstFixture: NextFixture = {
+            id: getFirstFixture.id,
+            localteam_id: getFirstFixture.localteam_id,
+            visitorteam_id: getFirstFixture.visitorteam_id,
+            starting_at: dayjs(getFirstFixture.starting_at).unix(),
+        };
         return firstFixture;
     } catch (error) {
         console.log("Error fetching data: ", error);
@@ -39,19 +53,16 @@ async function fetchNextFixtureData() {
     }
 }
 
-function signFixtureData(firstFixture: NextFixture) {
-    const signature = client.signMessage(
-        JSON.stringify(firstFixture),
-        PRIVATE_KEY
-    );
+function signFixtureData(fixture: NextFixture) {
+    const signature = client.signMessage(JSON.stringify(fixture), PRIVATE_KEY);
 
     return {
         data: {
-            fixtureID: firstFixture.id,
-            localTeamID: firstFixture.localteam_id,
-            visitorTeamID: firstFixture.visitorteam_id,
-            startingAt: firstFixture.starting_at,
-            timestamp: dayjs(new Date()),
+            fixtureID: fixture.id,
+            localTeamID: fixture.localteam_id,
+            visitorTeamID: fixture.visitorteam_id,
+            startingAt: fixture.starting_at,
+            timestamp: dayjs(new Date()).unix(),
         },
         signature: signature.signature,
         publicKey: signature.publicKey,
